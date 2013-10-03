671f62a9dc333a32438135bc70d1eb16e3a59db1
Crystal McDonald <stalmcdonald@fullsail.edu>
2013-10-03 15:53:47 -0700

    setup Onclick Listener for lntent Launch (working)
    Made changes to to GreatLauncher Class

--- src/com/cm/thegreatlauncher/GreatLauncher.java
+++ src/com/cm/thegreatlauncher/GreatLauncher.java
@@ -6,29 +6,47 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class GreatLauncher extends Activity {
-
+public class GreatLauncher extends Activity implements OnClickListener{
+	Button browseButton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.launcher);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.launcher, menu);
-		return true;
-	}
-	
-	public void showWebPage(View view) {
-    	Intent intent = new Intent(Intent.ACTION_VIEW,
-				Uri.parse("http://sfsailing.com/sailing/index.cfm"));
-    	
-    	startActivity(intent);
-    }
+		
+		browseButton = (Button) findViewById(R.id.browseButton);
+		
+		browseButton.setOnClickListener(new OnClickListener() {//setup onclick listener
+		
+			/*
+		 * Implicit Intent: Launches Web Browsers to open website
+		 */
+			public void onClick(View webview) {
+				// sends user to site that shows more information on sailing in San Francisco
+	Intent viewIntent = new Intent("android.intent.action.VIEW", Uri.parse("http://sfsailing.com/sailing/index.cfm"));
+	startActivity(viewIntent); 
+};	
+		});
 }
+		@Override
+		public boolean onCreateOptionsMenu(Menu menu) {
+			// Inflate the menu; this adds items to the action bar if it is present.
+			getMenuInflater().inflate(R.menu.launcher, menu);
+			return true;
+			
+		}
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+}//end
+	
 
 
 	
